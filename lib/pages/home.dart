4f55340c44e9c37b2e640239adee0500204dddd0
Depazanueva <samuel.depaz22@itca.edu.sv>
2023-10-03 05:58:43 -0600

    agregando apartado (AN)

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import 'acerca_nosotros.dart';
 import 'crear_datos.dart';
 
 class MyHomePage extends StatefulWidget {
